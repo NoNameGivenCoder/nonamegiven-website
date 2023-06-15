1440db36a0a90688c6ee0c1c7134e55826ff4b6f
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2023-06-14 22:33:58 -0700

    added favicon and a bit more, also og:image

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function copyMiiData() {
+    navigator.clipboard.writeText("AwAAQEp1suGiJbAC3DXFNXg5ODM4EQAAomNOAG8AUABuAGkAZAAAAAAAAAAAAGIqAJBCARFoRBgpFEYUgRIXaA0AACkAUkhUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPfy")
+    alert("Mii Data has been saved to clipboard! ^_^")
+}
