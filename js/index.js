ce1eb31d050277a1f2995ded3c3c9c6f92c10dcf
chayisslay <114644239+chayisslay@users.noreply.github.com>
2023-07-07 12:05:01 -0700

    everything should be in stable condition now

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 function copyMiiData() {
-    navigator.clipboard.writeText("AwAAQEp1suGiJbAC3DXFNXg5ODM4EQAAomNOAG8AUABuAGkAZAAAAAAAAAAAAGIqAJBCARFoRBgpFEYUgRIXaA0AACkAUkhUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPfy")
-    alert("Mii Data has been saved to clipboard! ^_^")
+    navigator.clipboard
+        .writeText("AwAAQEp1suGiJbAC3DXFNXg5ODM4EQAAomNOAG8AUABuAGkAZAAAAAAAAAAAAGIqAJBCARFoRBgpFEYUgRIXaA0AACkAUkhUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPfy")
+        .then(() => {
+            alert("Mii Data has been saved to clipboard! ^_^")
+        })
+        .catch(() => {
+            alert("Sorry! Something went wrong :(")
+        })
+    
 }
