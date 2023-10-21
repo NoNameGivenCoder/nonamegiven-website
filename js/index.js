8510085c3fc3b96bf096158b571ec591771d5348
chayisslay <114644239+chayisslay@users.noreply.github.com>
2023-10-20 20:55:41 -0700

    blog template

--- js/index.js
+++ js/index.js
@@ -17,6 +17,23 @@ async function CreateNav() {
     body.prepend(navFetch)
 }
 
-function CreateBlog() {
-    
+async function CreateBlog() {
+    var blogFetch = JSON.parse(await (await (await fetch("https://nonamegiven.xyz/blog.json")).blob()).text())
+    var templateBlog = await (await (await fetch("https://nonamegiven.xyz/blog_template.html")).blob().text())
+
+    for (let i = 0; i < blogFetch.length; i++) {
+        const blog_post = blogFetch[i];
+        var blog_html = templateBlog
+
+        console.log("Prepending " + blog_post.title + " to current HTML.")
+
+        blog_html.replace("[title]", blog_post.title)
+        blog_html.replace("[date]", blog_post.date)
+        blog_html.replace("[description]", blog_post.description)
+        blog_html.replace("[body]", blog_post.body)
+
+        $("body").prepend(blog_html)
+    }
+
+    console.log("Finished prepending all blog posts.")
 }
